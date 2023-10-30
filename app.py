a2fba3ee5238392311cb06cf5bddfc1f99d3c8fe
wbarobinson <wbarobinson@gmail.com>
2023-10-30 14:14:08 -0400

    first draft

---
+++ app.py
@@ -0,0 +1,81 @@
+import sqlite3
+import requests
+import time
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the FID from the environment variable
+viewer_fid = os.getenv("FARCASTER_DEVELOPER_FID")
+api_key = os.getenv("NEYNAR_API_KEY")
+
+# Initialize Database Connection
+conn = sqlite3.connect('farcaster_users.db')
+cursor = conn.cursor()
+
+# Create Table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    fid INTEGER PRIMARY KEY,
+    username TEXT,
+    follower_count INTEGER,
+    following_count INTEGER,
+    following BOOLEAN,
+    followed_by BOOLEAN
+)
+''')
+
+# Function to Check if User Data Already Exists
+def user_data_exists(fid):
+    cursor.execute('SELECT 1 FROM users WHERE fid = ?', (fid,))
+    return cursor.fetchone() is not None
+
+# Function to Insert Data into Database
+def insert_user_data(user_data):
+    cursor.execute('''
+    INSERT INTO users (fid, username, follower_count, following_count, following, followed_by)
+    VALUES (?, ?, ?, ?, ?, ?)
+    ''', (user_data['fid'], user_data['username'], user_data['followerCount'], 
+          user_data['followingCount'], user_data['viewerContext']['following'], 
+          user_data['viewerContext']['followedBy']))
+    conn.commit()
+
+# Example API Call to Fetch User Data
+def fetch_user_data(fid):
+    if user_data_exists(fid):
+        print(f"Data for FID {fid} already exists. Skipping.")
+        return
+
+    url = f"https://api.neynar.com/v1/farcaster/user?fid={fid}&viewerFid={viewer_fid}"
+    headers = {
+        "accept": "application/json",
+        "api_key": api_key  # Replace with your actual API key
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        user_data = response.json().get('result', {}).get('user', {})
+        insert_user_data(user_data)
+        print(f"Data fetched for FID {fid}.")
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP Error for FID {fid}: {e}")
+        if response.status_code == 429:  # Typically, 429 is the status code for rate limits
+            print("Rate limit hit. Waiting before next request...")
+            time.sleep(60)  # Wait for 60 seconds before the next request
+    except Exception as e:
+        print(f"Error fetching data for FID {fid}: {e}")
+
+# Fetch and Insert Data for a Range of Users
+for fid in range(0, 100001):
+    fetch_user_data(fid)
+    time.sleep(0.1)  # Introduce a small delay between requests to avoid hitting rate limits
+
+# Query Database for Users Sorted by Follower Count
+cursor.execute('SELECT * FROM users ORDER BY follower_count DESC')
+for row in cursor.fetchall():
+    print(row)
+
+# Close Database Connection
+conn.close()
---
+++ followerscript.py
@@ -0,0 +1,70 @@
+import sqlite3
+import requests
+import time
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the API Key from the environment variable
+api_key = os.getenv("NEYNAR_API_KEY")  # Ensure you have your API key in the .env file
+signer_uuid = os.getenv("NEYNAR_FARCASTER_UUID")
+
+def follow_user(fid, cursor, api_key, signer_uuid, conn):
+    print("API Key:", api_key)
+    print("Signer UUID:", signer_uuid)
+
+    url = "https://api.neynar.com/v2/farcaster/user/follow"
+    headers = {
+        "accept": "application/json",
+        "api_key": api_key,
+        "content-type": "application/json"
+    }
+    payload = {
+        "signer_uuid": signer_uuid,
+        "target_fids": [fid]
+    }
+
+    print("Payload:", payload)  # Debugging
+
+    try:
+        response = requests.post(url, json=payload, headers=headers)
+        response.raise_for_status()
+        print(f"Successfully followed user: {fid}")
+        update_following_status(fid, cursor, conn)
+    except requests.exceptions.HTTPError as e:
+        print(f"Error following user {fid}: {e}")
+        print("Response content:", response.content.decode())
+
+def update_following_status(fid, cursor, conn):
+    cursor.execute('''
+    UPDATE users SET following = 1 WHERE fid = ?
+    ''', (fid,))
+    conn.commit()
+
+def follow_users(fids, cursor, api_key, signer_uuid, conn):
+    for fid in fids:
+        follow_user(fid, cursor, api_key, signer_uuid, conn)
+        time.sleep(1)  # Pause for 1 second between each follow request
+
+def main():
+    # Initialize Database Connection
+    conn = sqlite3.connect('farcaster_users.db')
+    cursor = conn.cursor()
+
+    # Define the starting point (the user id where it stopped)
+    start_from_id = 1
+
+    # Fetch Users with More Than 100 Followers and Not Already Followed, starting from start_from_id
+    cursor.execute('SELECT fid FROM users WHERE fid > ? AND follower_count BETWEEN 25 AND 100 AND following = 0', (start_from_id,))
+    fids_to_follow = [row[0] for row in cursor.fetchall()]
+
+    # Follow the Users
+    follow_users(fids_to_follow, cursor, api_key, signer_uuid, conn)
+
+    # Close Database Connection
+    conn.close()
+
+if __name__ == "__main__":
+    main()
